8b139061dcf336eebe176aec7bf05d9f2bbffa53
caileighf <caileighfitzgerald@gmail.com>
2020-06-25 13:52:27 -0400

    added feature in clean script for passing in a data directory rather than assuming the default or <cwd>/data

--- clean.sh
+++ clean.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
-rm data/*.txt && echo "Deleted Data Files"
 
+# Script to clear all data files from <cwd>/data
+# User can change the data directory by passing the path
+DATA_DIR="./data"
+
+# check if arg passed
+if [ ! -z "$1" ] ; then
+    DATA_DIR="$1"
+fi
+
+if rm $DATA_DIR/*.txt > /dev/null 2>&1 ; then
+    echo "Deleted Data Files"
+else
+    echo "Could not delete data files"
+    echo "Possible reasons:"
+    echo "-----------------"
+    echo "1. The data directory is empty"
+    echo "2. You do not have the necessary permissions for [$DATA_DIR]"
+    echo "3. [$DATA_DIR] does not exist/path is incorrect"
+fi
