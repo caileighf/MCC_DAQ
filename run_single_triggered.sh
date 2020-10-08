8cc52f5816bdd15fcf1f69da490ad6867ac44440
caileighf <caileighfitzgerald@gmail.com>
2020-10-08 13:04:48 -0400

    Changed  bash var initialization to append to existing  - now this script will work on the dev machine and another machine with the PYTHON_EXE var already set

--- run_single_triggered.sh
+++ run_single_triggered.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-PYTHON_EXE="/home/caileigh/repos/venvs/testMCC/bin/python"
+PYTHON_EXE=$PYTHON_EXE:/home/caileigh/repos/venvs/testMCC/bin/python
 
 $PYTHON_EXE scripts/single_DAQ_collect_trigger.py --use-config --script
